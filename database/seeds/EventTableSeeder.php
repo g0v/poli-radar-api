2ad916147373d3ad74b503d1ab90677c3f67958a
Pomin Wu <pomin5@gmail.com>
2017-02-21 09:54:35 -0500

    Added event seeder using CSV

--- database/seeds/EventTableSeeder.php
+++ database/seeds/EventTableSeeder.php
@@ -29,45 +29,44 @@ class EventTableSeeder extends Seeder
         $photo = MediaType::create(['name' => '照片', 'slug' => 'photo']);
 
         $evt_cat = EventCategory::create(['name' => '立法委員']);
-        $cats_list = ['中央行程', '地方行程', '媒體行程'];
+        $cats_list = ['國會事務', '地方事務', '媒體公關'];
         $main_cats = array_reduce($cats_list, function($list, $cat_name) use ($evt_cat) {
           $sub_cat = EventCategory::create([
             'parent_id' => $evt_cat->id,
             'name' => $cat_name,
           ]);
-          foreach (range(0, 3) as $i) {
-            $list[] = EventCategory::create([
-              'parent_id' => $sub_cat->id,
-              'name' => $cat_name . ' - ' . ($i + 1),
-            ]);
-          }
           return $list;
         }, []);
         $poli_cat = PostClassification::where('name', '立法委員')->first();
         $evt_cat->post_classification()->save($poli_cat);
 
-        $faker = \Faker\Factory::create('zh_TW');
+        foreach (['余宛如', '吳秉叡', '洪慈庸', '蔣萬安'] as $personName) {
 
-        foreach (Person::all() as $person) {
-          foreach ($main_cats as $sub_cat) {
-            foreach (range(0, rand(2, 5)) as $i) {
-              $evt = Event::create([
-                'date' => $faker->dateTimeBetween('2016-02-01'),
-                'name' => $faker->realText($faker->numberBetween(10,20)),
-                'description' => $faker->realText($faker->numberBetween(40,100)),
-                'user_id' => 1,
-              ]);
-              $evt->persons()->attach($person);
-              $evt->categories()->attach($sub_cat);
-              if (rand(0, 10) > 7) {
-                $media = Media::create([
-                  'event_id' => $evt->id,
-                  'type_id' => $photo->id,
-                  'value' => $faker->imageUrl(),
+            $fh = fopen(__DIR__ . '/csv/政治人物行程雷達探測行程表 - ' . $personName . '.csv', 'r');
+            $header = fgetcsv($fh);
+
+            $person = Person::where('name', $personName)->first();
+            while (($row = fgetcsv($fh)) !== false) {
+                $data = [];
+                foreach ($header as $index => $field) {
+                    $data[$field] = $row[$index];
+                }
+                $event = Event::create([
+                    'date' => Carbon::createFromFormat('Y-m-d', $data['行程日期']),
+                    'start' => preg_match('/^\d\d:\d\d:\d\d$/', $data['開始時間（選填）']) ? Carbon::createFromFormat('Y-m-d H:i:s', $data['行程日期'] . ' ' . $data['開始時間（選填）']) : null,
+                    'end' => preg_match('/^\d\d:\d\d:\d\d$/', $data['結束時間（選填）']) ? Carbon::createFromFormat('Y-m-d H:i:s', $data['行程日期'] . ' ' . $data['結束時間（選填）']) : null,
+
+                    'name' => $data['行程名稱'],
+                    'description' => $data['行程敘述（選填）'],
+                    'link' => $data['相關連結（選填）'] ?: null,
+                    'user_id' => 1,
                 ]);
-              }
+                $event->persons()->attach($person);
+
+                $cat = EventCategory::where('name', $data['行程分類'])->first();
+                $event->categories()->attach($cat);
             }
-          }
+            fclose($fh);
         }
     }
 }
